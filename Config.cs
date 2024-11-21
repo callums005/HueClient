068d145adfe142cfc9a9ff55f4ab9651fc118b81
Callum <dev@callumstevenson.net>
2024-11-21 17:48:47 +0000

    Inital commit

---
+++ Config.cs
@@ -0,0 +1,8 @@
+namespace HueClient;
+
+public class Config
+{
+	public string baseUrl { get; init; }
+	public string username { get; init; }
+	public Dictionary<string, string> urls { get; init; }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace HueClient;
+
+class Program
+{
+	private static HttpClient _httpClient { get; set; }
+
+	public static void DisplayError(string message)
+	{
+		Console.ForegroundColor = ConsoleColor.White;
+		Console.BackgroundColor = ConsoleColor.Red;
+		Console.Write("[ERR]");
+		Console.ResetColor();
+		Console.Write(" ");
+		Console.ForegroundColor = ConsoleColor.Red;
+		Console.WriteLine(message);
+		Console.ResetColor();
+	}
+
+	public static Config? FetchConfig()
+	{
+		try
+		{
+			using StreamReader reader = new("config.json");
+			string json = reader.ReadToEnd();
+
+			Config? config = JsonSerializer.Deserialize<Config>(json);
+
+			return config;
+		}
+		catch (Exception e)
+		{
+			DisplayError(e.Message);
+		}
+
+		return null;
+	}
+
+	public static async Task Main(string[] args)
+	{
+		System.Net.ServicePointManager.SecurityProtocol =
+			System.Net.SecurityProtocolType.Tls12 |
+			System.Net.SecurityProtocolType.Tls13;
+
+		HttpClientHandler handler = new HttpClientHandler
+		{
+			ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true
+		};
+
+		_httpClient = new(handler);
+
+		Config? config = FetchConfig();
+
+		if (config is null)
+		{
+			DisplayError("Failed to fetch config file data.");
+			return;
+		}
+
+		_httpClient.DefaultRequestHeaders.Add("hue-application-key", Environment.GetEnvironmentVariable("hueClient.clientSecret"));
+
+		string url = $"{config.baseUrl}{config.urls["getDevices"]}";
+
+		try
+		{
+			HttpResponseMessage response = await _httpClient.GetAsync(url);
+			Console.WriteLine(response.StatusCode);
+			Console.WriteLine(await response.Content.ReadAsStringAsync());
+		}
+		catch (Exception e)
+		{
+			DisplayError(e.Message);
+		}
+	}
+}
